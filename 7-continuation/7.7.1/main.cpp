fd16ac4c73e93293a41b434ad58f835c1974ad06
7shi <7shi@live.jp>
2011-12-01 00:55:51 +0900

    サンプルを差し替え

--- 7-continuation/7.7.1/main.cpp
+++ 7-continuation/7.7.1/main.cpp
@@ -1,15 +1,26 @@
 #include <iostream>
+#include <string>
 #include "Coroutine.h"
 
 using namespace std;
 
-void test() {
-    for (int i = 0; i <= 5; i++)
-        yield(i);
+void test1() {
+    yield<string>("abc");
+    yield<string>("1234");
+    yield<string>("finish!");
+}
+
+void test2() {
+    Coroutine<string> cr = test1;
+    while (cr()) {
+        auto s = cr.value;
+        cout << s << ": ";
+        yield<int>(s.size());
+    }
 }
 
 int main() {
-    Coroutine<int> cr = test;
+    Coroutine<int> cr = test2;
     while (cr())
         cout << cr.value << endl;
 }
